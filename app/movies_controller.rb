ac9a4ceaaa60e96a3ebdb7cb4cf01af6debdfaf8
Ron Sala <ron.sala@gmail.com>
2018-06-12 23:04:02 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -45,20 +45,22 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find(1).title
 end
 
 def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  # Movie.having(release_date: >2002 DESC)
+  # Movie.where(release_date: 2002..2018 :desc)
+  movies = Movie.where(release_date: 2003..2018)
 end
 
 def can_be_found_updated_and_saved
